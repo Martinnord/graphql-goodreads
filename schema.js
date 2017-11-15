34dcb8d61853ba9dda9afd98598c2109c3ab931e
martinnord <martin.nordstrom99@gmail.com>
2017-11-15 18:57:08 +0100

    can get data from the crappy xml api from Goodreads via GraphQL

---
+++ schema.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch')
+const util = require('util')
+const parseXML = util.promisify(require('xml2js').parseString)
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString
+} = require('graphql')
+
+  const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: '',
+
+    fields: () => ({
+      name: {
+        type: GraphQLString,
+        resolve: xml =>
+          xml.GoodreadsResponse.author[0].name[0]
+      }
+    })
+  })
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: '',
+
+    fields: () => ({
+      author: {
+        type: AuthorType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args) => fetch(
+          `https://www.goodreads.com/author/show.xml?id=${args.id}&key=rb7SjYvl3qie0JBW91Kmg`
+        )
+          .then(res => res.text())
+          .then(parseXML)
+      }
+    })
+  })
+})
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const app = express()
+
+const schema = require('./schema')
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+app.listen(8000)
+console.log('Listening on port 8000')
